9f3f750a3d22baee4548f96caf10608eafedb14f
lys <lys9229@naver.com>
2025-11-28 14:43:33 +0900

    get m

--- src/main/java/kr/soft/git/MemberController.java
+++ src/main/java/kr/soft/git/MemberController.java
@@ -1,8 +1,14 @@
 package kr.soft.git;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class MemberController {
+    @GetMapping("/d")
+    public void getD() {
+        System.out.println("dddddd");
+    }
 
 }
