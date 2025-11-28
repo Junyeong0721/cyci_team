92a9aa9bfe8def792342da947220652b89974183
ys <jbaoro02@gmail.com>
2025-11-28 14:41:01 +0900

    jb

--- src/main/java/kr/soft/git/MemberController.java
+++ src/main/java/kr/soft/git/MemberController.java
@@ -1,8 +1,16 @@
 package kr.soft.git;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 public class MemberController {
+    @GetMapping("/jb")
+    public void jb(){
+        log.info("jb");
+    }
+
 
 }
